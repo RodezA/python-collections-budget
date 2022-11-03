6799ee462f0965835d46c433afccf0039fce7def
anthony.rodez <anthony.rodez@corellium.com>
2022-11-03 09:28:26 -0600

    Lesson 5 updates

--- budget/Expense.py
+++ budget/Expense.py
@@ -40,3 +40,9 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+    def categorize_set_comprehension(self):
+        self.necessary_expenses = {if x for x in self.list}
+            if x.category == ('Phone' or x.category) or x.category != 'Auto and Gas' or x.category
+        self.food_expenses
+                
